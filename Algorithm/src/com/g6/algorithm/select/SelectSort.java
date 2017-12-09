71baa388cc48483a156a0aea3080412cc51e59ea
lv huixiao <huahangxiaohui@163.com>
2017-12-09 13:14:39 +0800

    A#选择排序算法提交#

--- Algorithm/src/com/g6/algorithm/select/SelectSort.java
+++ Algorithm/src/com/g6/algorithm/select/SelectSort.java
@@ -4,7 +4,27 @@ package com.g6.algorithm.select;
  * Created by huixiaolv on 09/12/2017.
  */
 public class SelectSort {
-    public static void main(String[] args){
 
+    public static int[] selectSort(int[] array){
+        int len = array.length;
+        for(int i=0;i<len;i++){
+            int minIndex = i;
+            for(int j=i+1;j<len;j++){
+                if(array[j]<array[i])
+                    minIndex=j;
+            }
+            int swap = array[i];
+            array[i]=array[minIndex];
+            array[minIndex]=swap;
+        }
+        return array;
+    }
+
+    public static void main(String[] args){
+        int[] array = {10,9,8,7,6,5,4,3,2,1};
+        int[] ints = selectSort(array);
+        for(int i=0;i<ints.length;i++){
+            System.out.println(ints[i]);
+        }
     }
 }
